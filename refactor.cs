f0a5fbb38b2e2933e7c610a4f49c03dfa7a12339
Jason9292 <jason41@mail.usf.edu>
2022-07-06 10:50:00 -0400

    Add cinnamon ingredient fix variables that were incorrectly named banana

--- refactor.cs
+++ refactor.cs
@@ -6,7 +6,7 @@ namespace Interview_Refactor1
         static void Main(string[] args)
         {
             // want to maximize the number of apple pies we can make.
-            // it takes 3 apples, 2 lbs of sugar and 1 pound of flour to make 1 apple pie
+            // it takes 3 apples, 2 lbs of sugar, 1 pound of flour, and 2 teaspoons of cinnamon to make 1 apple pie
             // this is intended to run on .NET Core
             do
             {
@@ -31,32 +31,40 @@ namespace Interview_Refactor1
                     continue;
                 }
 
-                generateApplePie.PieIngredients pieIngredients = generateApplePie.GeneratePieValues(apples, sugar, flour);
+                Console.WriteLine("How many teaspoons of cinnamon do you have?");
+                int cinnamon;
+                if (!int.TryParse(Console.ReadLine(), out cinnamon)){
+                    Console.WriteLine($"Please enter value in numerical format. Press enter to start over, 'q' to quit!");
+                    continue;
+                }
+
+                generateApplePie.PieIngredients pieIngredients = generateApplePie.GeneratePieValues(apples, sugar, flour, cinnamon);
                 Console.WriteLine("You can make: " + pieIngredients.maxPies + " apple pies!");
-                Console.WriteLine(pieIngredients.leftOverApples + " apple(s) left over, " + pieIngredients.leftOverBananas + " lbs sugar left over, " + pieIngredients.leftOverFlour + " lbs flour left over.");
+                Console.WriteLine(pieIngredients.leftOverApples + " apple(s) left over, " + pieIngredients.leftOverSugar + " lbs sugar left over, " + pieIngredients.leftOverFlour + " lbs flour left over, " + pieIngredients.leftOverCinnamon + " teaspoons cinnamon left over.");
                 Console.WriteLine("\n\nEnter to calculate, 'q' to quit!");
             } while (!string.Equals(Console.ReadLine().ToUpper(), "Q"));
         }
     }
     public static class generateApplePie
     {
-        public static PieIngredients GeneratePieValues(int apples, int bananas, int flour)
+        public static PieIngredients GeneratePieValues(int apples, int sugar, int flour, int cinnamon)
         {
             PieIngredients pieIngredients = new PieIngredients();
 
-            pieIngredients.maxPies = Math.Min(Math.Min((apples/3), (bananas/2)), flour);
+            pieIngredients.maxPies = Math.Min(Math.Min((apples/3), (sugar/2)), Math.Min(flour, (cinnamon/2)));
             pieIngredients.leftOverApples = apples - (pieIngredients.maxPies * 3);
-            pieIngredients.leftOverBananas = bananas - (pieIngredients.maxPies * 2);
+            pieIngredients.leftOverSugar = sugar - (pieIngredients.maxPies * 2);
             pieIngredients.leftOverFlour = flour - pieIngredients.maxPies;
-
+            pieIngredients.leftOverCinnamon = cinnamon - (pieIngredients.maxPies * 2);
             return pieIngredients;
         }
 
         public class PieIngredients{
             public int maxPies;
             public int leftOverApples;
-            public int leftOverBananas;
+            public int leftOverSugar;
             public int leftOverFlour;
+            public int leftOverCinnamon;
         }
     }
 
