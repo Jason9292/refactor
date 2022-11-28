3d0b4ee763096a86c704108998faa11fd2622d7f
Jason Mellinger <JMellinger@ConnectWise.com>
2022-11-28 09:33:30 -0500

    add tests

--- PieCalcTest/UnitTest1.cs
+++ PieCalcTest/UnitTest1.cs
@@ -9,14 +9,29 @@ namespace PieCalcTest
         [TestMethod]
         public void TestMaxPiesGenerated()
         {
-            ApplePieGenerator.PieValues pieValues = ApplePieGenerator.GeneratePieValues(3, 2, 1, 2, 8);
-            Assert.AreEqual(pieValues.maxCinnamonPies, 1);
-            Assert.AreEqual(pieValues.maxRegularPies, 0);
-            Assert.AreEqual(pieValues.leftOverApples, 0);	
-            Assert.AreEqual(pieValues.leftOverSugar, 0);
-            Assert.AreEqual(pieValues.leftOverFlour, 0);
-            Assert.AreEqual(pieValues.leftOverCinnamon, 0);
-            Assert.AreEqual(pieValues.leftOverButter, 2);
+            List<RecipeIngredientCalculator.Ingredient> ingredients = new List<RecipeIngredientCalculator.Ingredient>();
+            ingredients.Add(new RecipeIngredientCalculator.Ingredient(){ quantity = 2, measurementUnit = "cups", name = "orange juice"});
+
+            List<RecipeIngredientCalculator.Ingredient> recipe = new List<RecipeIngredientCalculator.Ingredient>();
+            recipe.Add(new RecipeIngredientCalculator.Ingredient(){ quantity = 1, measurementUnit = "cups", name = "orange juice"});
+
+            int maxAmount = RecipeIngredientCalculator.CalculateMaxAmount(ingredients, recipe);
+
+            Assert.AreEqual(maxAmount, 2);
+        }
+
+        [TestMethod]
+        public void TestMaxPiesGenerated()
+        {
+            List<RecipeIngredientCalculator.Ingredient> ingredients = new List<RecipeIngredientCalculator.Ingredient>();
+            ingredients.Add(new RecipeIngredientCalculator.Ingredient(){ quantity = 2, measurementUnit = "cups", name = "orange juice"});
+
+            List<RecipeIngredientCalculator.Ingredient> recipe = new List<RecipeIngredientCalculator.Ingredient>();
+            recipe.Add(new RecipeIngredientCalculator.Ingredient(){ quantity = 1, measurementUnit = "cups", name = "orange juice"});
+
+            List<RecipeIngredientCalculator.Ingredient> leftOvers = RecipeIngredientCalculator.CalculateLeftovers(ingredients, recipe, 1);
+
+            Assert.AreEqual(leftOvers[0].quantity, 1);
         }
     }
 }
